8be72a16780c59a5fc750fa41534f0844dc56dd2
Robert Ignat <robert.ignat91@gmail.com>
2020-03-20 16:29:16 +0200

    Add struct attr generator

--- src/attrs/mod.rs
+++ src/attrs/mod.rs
@@ -1,1 +1,67 @@
+use quote::quote;
+use syn::{parse2, Attribute, ItemStruct, Meta};
+
+use crate::args::{Args, Attrs, Doc};
+use crate::error::unexpected;
+
 pub mod generator;
+
+use generator::AttrGenerator;
+
+struct AttrGen<'a> {
+    item: &'a ItemStruct,
+    args: &'a Args,
+}
+
+impl<'a> AttrGen<'a> {
+    fn new(item: &'a ItemStruct, args: &'a Args) -> Self {
+        Self { item, args }
+    }
+}
+
+impl<'a> AttrGenerator for AttrGen<'a> {
+    fn no_docs(&self) -> bool {
+        self.args.doc.is_none()
+    }
+
+    fn error_action_text(&self) -> String {
+        format!("generating {} attrs", self.item.ident)
+    }
+
+    fn original_attrs(&self) -> &[Attribute] {
+        &self.item.attrs
+    }
+
+    fn attrs_arg(&self) -> &Option<Attrs> {
+        &self.args.attrs
+    }
+
+    fn custom_docs(&self) -> Option<Meta> {
+        if let Some(Doc::Custom(d)) = &self.args.doc {
+            let tokens = quote! {
+                doc = #d
+            };
+
+            Some(parse2(tokens).unwrap_or_else(|e| panic!(unexpected(self.error_action_text(), e))))
+        } else {
+            None
+        }
+    }
+
+    fn keep_original_docs(&self) -> bool {
+        use Doc::*;
+
+        match self.args.doc {
+            None | Some(Custom(_)) => false,
+            Some(Same) => true,
+        }
+    }
+}
+
+pub fn generate(item: &mut ItemStruct, args: &Args) {
+    item.attrs = {
+        let gen = AttrGen::new(item, args);
+
+        gen.generate()
+    };
+}
--- src/generate.rs
+++ src/generate.rs
@@ -3,7 +3,7 @@ use quote::quote;
 use syn::ItemStruct;
 
 use crate::args::Args;
-use crate::{docs, fields};
+use crate::{attrs, fields};
 
 pub fn generate(args: Args, original: &ItemStruct) -> TokenStream {
     let mut opt_struct = original.clone();
@@ -11,7 +11,7 @@ pub fn generate(args: Args, original: &ItemStruct) -> TokenStream {
     opt_struct.ident = args.name.clone();
     opt_struct.vis = args.final_visibility();
 
-    docs::generate(&mut opt_struct, &args);
+    attrs::generate(&mut opt_struct, &args);
     fields::generate(&mut opt_struct, &args);
 
     quote!(#opt_struct)

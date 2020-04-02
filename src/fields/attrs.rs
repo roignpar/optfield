d0525c4116d7a3d3792b5db18ec515ab7a5f6c05
Robert Ignat <robert.ignat91@gmail.com>
2020-04-02 18:14:15 +0300

    Add field attrs unit tests

--- src/fields/attrs.rs
+++ src/fields/attrs.rs
@@ -40,3 +40,192 @@ impl<'a> AttrGenerator for FieldAttrGen<'a> {
 pub fn generate(field: &Field, args: &Args) -> Vec<Attribute> {
     FieldAttrGen::new(field, args).generate()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use quote::quote;
+
+    use crate::test_util::*;
+
+    #[test]
+    fn remove_docs() {
+        let field = parse_field(quote! {
+            /// some
+            /// doc
+            /// lines
+            #[attr]
+            field: i32
+        });
+
+        let cases = vec![
+            quote! {
+                Opt
+
+            },
+            quote! {
+                Opt,
+                field_attrs
+            },
+            quote! {
+                Opt,
+                field_attrs = (new_attr)
+            },
+            quote! {
+                Opt,
+                field_attrs = add(new_attr)
+            },
+        ];
+
+        let item_docs = doc_attrs(&field.attrs);
+
+        for case in cases {
+            let args = parse_args(case);
+
+            let generated = generate(&field, &args);
+
+            assert!(!attrs_contain_any(&generated, &item_docs));
+        }
+    }
+
+    #[test]
+    fn keep_docs() {
+        let field = parse_field(quote! {
+            /// field
+            /// with
+            /// docs
+            #[attr]
+            field: String
+        });
+
+        let cases = vec![
+            quote! {
+                Opt,
+                field_docs
+            },
+            quote! {
+                Opt,
+                field_docs,
+                field_attrs
+            },
+            quote! {
+                Opt,
+                field_docs,
+                field_attrs = (new_attr)
+            },
+            quote! {
+                Opt,
+                field_docs,
+                field_attrs = add(new_attr)
+            },
+        ];
+
+        let item_docs = doc_attrs(&field.attrs);
+
+        for case in cases {
+            let args = parse_args(case);
+
+            let generated = generate(&field, &args);
+
+            assert!(attrs_contain_all(&generated, &item_docs));
+        }
+    }
+
+    #[test]
+    fn remove_attrs() {
+        let (field, args) = parse_field_and_args(
+            quote! {
+                #[some]
+                #[attrs]
+                field: String
+            },
+            quote! {
+                Opt
+            },
+        );
+
+        let generated = generate(&field, &args);
+
+        assert!(!attrs_contain_any(&generated, &field.attrs));
+    }
+
+    #[test]
+    fn keep_attrs() {
+        let (field, args) = parse_field_and_args(
+            quote! {
+                #[some]
+                #[attrs]
+                field: String
+            },
+            quote! {
+                Opt,
+                field_attrs
+            },
+        );
+
+        let generated = generate(&field, &args);
+
+        assert!(attrs_contain_all(&generated, &field.attrs));
+    }
+
+    #[test]
+    fn replace_attrs() {
+        let (field, args) = parse_field_and_args(
+            quote! {
+                #[some]
+                #[attrs]
+                field: String
+            },
+            quote! {
+                Opt,
+                field_attrs = (
+                    other,
+                    new,
+                    attribute
+                )
+            },
+        );
+
+        let new_attrs = parse_attrs(quote! {
+            #[other]
+            #[new]
+            #[attribute]
+        });
+
+        let generated = generate(&field, &args);
+
+        assert!(!attrs_contain_any(&generated, &field.attrs));
+        assert!(attrs_contain_all(&generated, &new_attrs));
+    }
+
+    #[test]
+    fn add_attrs() {
+        let (field, args) = parse_field_and_args(
+            quote! {
+                #[old]
+                #[attrs]
+                field: u8
+            },
+            quote! {
+                Opt,
+                field_attrs = add(
+                    new,
+                    field,
+                    attributes
+                )
+            },
+        );
+
+        let new_attrs = parse_attrs(quote! {
+            #[new]
+            #[field]
+            #[attributes]
+        });
+
+        let generated = generate(&field, &args);
+
+        assert!(attrs_contain_all(&generated, &field.attrs));
+        assert!(attrs_contain_all(&generated, &new_attrs));
+    }
+}

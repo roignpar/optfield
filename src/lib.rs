9991c53a7a880c5d8d4f26a20a7ddd1c56bcfe05
Robert Ignat <robert.ignat91@gmail.com>
2020-04-03 18:50:06 +0300

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,402 @@
+//! `optfield` is a macro that generates structs with optional fields from
+//! existing structs.
+//!
+//! # Simple examples
+//! The first argument is the name of the generated struct:
+//! ```
+//! use optfield::optfield;
+//!
+//! #[optfield(Opt)]
+//! struct MyStruct {
+//!     text: String
+//! }
+//! ```
+//! Will generate the following __second__ struct (leaving `MyStruct` as is):
+//! ```
+//! struct Opt {
+//!     text: Option<String>
+//! }
+//! ```
+//! It also works with tuple structs:
+//! ```
+//! # use optfield::*;
+//! #[optfield(Opt)]
+//! struct MyTuple(String, i32);
+//! ```
+//! Will generate:
+//! ```
+//! struct Opt(Option<String>, Option<i32>);
+//! ```
+//! Generics and lifetimes are preserved:
+//! ```
+//! # use optfield::*;
+//! #[optfield(Opt)]
+//! struct MyStruct<'a, T> {
+//!     field: &'a T
+//! }
+//! ```
+//! Will generate:
+//! ```
+//! struct Opt<'a, T> {
+//!     field: Option<&'a T>
+//! }
+//! ```
+//!
+//! # Visibility
+//! By default, opt structs are private. To use custom visibility simply pass
+//! it as an argument:
+//! ```
+//! # use optfield::*;
+//! #[optfield(Opt, pub(crate))]
+//! struct MyStruct{
+//!     text: String
+//! }
+//! ```
+//! Will generate:
+//! ```
+//! pub(crate) struct Opt {
+//!     text: Option<String>
+//! }
+//! ```
+//!
+//! # Rewrapping `Option` fields
+//! By default, fields that are already wrapped in `Option<T>` are not wrapped
+//! again:
+//! ```
+//! # use optfield::*;
+//! #[optfield(Opt)]
+//! struct MyStruct {
+//!     text: Option<String>,
+//!     number: i32
+//! }
+//! ```
+//! Will generate:
+//! ```
+//! struct Opt {
+//!     text: Option<String>,
+//!     number: Option<i32>
+//! }
+//! ```
+//! To rewrap them pass the `rewrap` argument:
+//!
+//! ```
+//! # use optfield::*;
+//! #[optfield(Opt, rewrap)]
+//! struct MyStruct {
+//!     text: Option<String>,
+//!     number: i32
+//! }
+//! ```
+//! Will generate:
+//! ```
+//! struct Opt {
+//!     text: Option<Option<String>>,
+//!     number: Option<i32>
+//! }
+//! ```
+//!
+//! # Documentation
+//! To document the opt struct, either duplicate the same documentation as the
+//! original using the `doc` argument by itself:
+//! ```
+//! # use optfield::*;
+//! /// My struct documentation
+//! /// ...
+//! # use optfield::*;
+//! #[optfield(Opt, doc)]
+//! struct MyStruct {
+//!     text: String
+//! }
+//! ```
+//! Will generate:
+//! ```
+//! /// My struct documentation
+//! /// ...
+//! struct Opt {
+//!     text: Option<String>
+//! }
+//! ```
+//! Or write custom documentation:
+//! ```
+//! # use optfield::*;
+//! #[optfield(
+//!     Opt,
+//!     doc = "
+//!         Custom documentation
+//!         for Opt struct...
+//!     "
+//! )]
+//! struct MyStruct {
+//!     text: String
+//! }
+//! ```
+//! Will generate:
+//! ```
+//! /// Custom documentation
+//! /// for Opt struct...
+//! struct Opt {
+//!     text: Option<String>
+//! }
+//! ```
+//!
+//! # Attributes
+//! The `attrs` argument makes optfield insert the same attributes as the
+//! original:
+//! ```
+//! # use optfield::*;
+//! #[optfield(Opt, attrs)]
+//! #[cfg(test)]
+//! #[derive(Clone)]
+//! struct MyStruct {
+//!     text: String
+//! }
+//! ```
+//! Will generate:
+//! ```
+//! #[cfg(test)]
+//! #[derive(Clone)]
+//! struct Opt {
+//!     text: Option<String>
+//! }
+//! ```
+//! To add more attributes besides the original ones, use `attrs = add(...)`:
+//! ```
+//! # use optfield::*;
+//! #[optfield(
+//!     Opt,
+//!     attrs = add(
+//!         cfg(test),
+//!         derive(Clone)
+//!     )
+//! )]
+//! #[derive(Debug)]
+//! struct MyStruct {
+//!     text: String
+//! }
+//! ```
+//! Will generate:
+//! ```
+//! #[derive(Debug)]
+//! #[cfg(test)]
+//! #[derive(Clone)]
+//! struct Opt {
+//!     text: Option<String>
+//! }
+//! ```
+//! To replace with other attributes:
+//! ```
+//! # use optfield::*;
+//! #[optfield(
+//!     Opt,
+//!     attrs = (
+//!         cfg(test),
+//!         derive(Clone)
+//!     )
+//! )]
+//! #[derive(Debug)]
+//! struct MyStruct {
+//!     text: String
+//! }
+//! ```
+//! Will generate:
+//! ```
+//! #[cfg(test)]
+//! #[derive(Clone)]
+//! struct Opt {
+//!     text: Option<String>
+//! }
+//! ```
+//!
+//! # Field documentation
+//! By default, field documentation is removed:
+//! ```
+//! # use optfield::*;
+//! #[optfield(Opt)]
+//! struct MyStruct {
+//!     /// Field
+//!     /// documentation
+//!     text: String
+//! }
+//! ```
+//! Will generate:
+//! ```
+//! struct Opt {
+//!     text: Option<String>
+//! }
+//! ```
+//! To preserve field documentation use the `field_doc` argument:
+//! ```
+//! # use optfield::*;
+//! #[optfield(Opt, field_doc)]
+//! struct MyStruct {
+//!     /// Field
+//!     /// documentation
+//!     text: String
+//! }
+//! ```
+//! Will generate:
+//! ```
+//! struct Opt {
+//!     /// Field
+//!     /// documentation
+//!     text: Option<String>
+//! }
+//! ```
+//!
+//! # Field attributes
+//! Field attributes can be handled using the `field_attrs` argument which works
+//! similarly to `attrs`, but applies to all fields.
+//!
+//! `field_attrs` can be used independently of `attrs`.
+//!
+//! By default, no field attributes are inserted:
+//! ```
+//! # use optfield::*;
+//! # use serde::Deserialize;
+//! #[optfield(Opt, attrs)]
+//! #[derive(Deserialize)]
+//! struct MyStruct {
+//!     #[serde(rename = "text")]
+//!     my_text: String
+//! }
+//! ```
+//! Will generate:
+//! ```
+//! # use serde::Deserialize;
+//! #[derive(Deserialize)]
+//! struct Opt {
+//!     my_text: Option<String>
+//! }
+//! ```
+//! To keep them:
+//! ```
+//! # use optfield::*;
+//! # use serde::Deserialize;
+//! #[optfield(Opt, attrs, field_attrs)]
+//! #[derive(Deserialize)]
+//! struct MyStruct {
+//!     #[serde(rename = "text")]
+//!     my_text: String
+//! }
+//! ```
+//! Will generate:
+//! ```
+//! # use serde::Deserialize;
+//! #[derive(Deserialize)]
+//! struct Opt {
+//!     #[serde(rename = "text")]
+//!     my_text: Option<String>
+//! }
+//! ```
+//! To add more attributes:
+//! ```
+//! # use optfield::*;
+//! # use serde::Deserialize;
+//! #[optfield(
+//!     Opt,
+//!     attrs,
+//!     field_attrs = add(
+//!         serde(default)
+//!     )
+//! )]
+//! #[derive(Deserialize)]
+//! struct MyStruct {
+//!     #[serde(rename = "text")]
+//!     my_text: String,
+//!     #[serde(rename = "number")]
+//!     my_number: i32
+//! }
+//! ```
+//! Will generate:
+//! ```
+//! # use serde::Deserialize;
+//! #[derive(Deserialize)]
+//! struct Opt {
+//!     #[serde(rename = "text")]
+//!     #[serde(default)]
+//!     my_text: Option<String>,
+//!     #[serde(rename = "number")]
+//!     #[serde(default)]
+//!     my_number: Option<i32>
+//! }
+//! ```
+//! To replace all field attributes:
+//! ```
+//! # use optfield::*;
+//! # use serde::Deserialize;
+//! #[optfield(
+//!     Opt,
+//!     attrs,
+//!     field_attrs = (
+//!         serde(default)
+//!     )
+//! )]
+//! #[derive(Deserialize)]
+//! struct MyStruct {
+//!     #[serde(rename = "text")]
+//!     my_text: String,
+//!     #[serde(rename = "number")]
+//!     my_number: i32
+//! }
+//! ```
+//! Will generate:
+//! ```
+//! # use serde::Deserialize;
+//! #[derive(Deserialize)]
+//! struct Opt {
+//!     #[serde(default)]
+//!     my_text: String,
+//!     #[serde(default)]
+//!     my_number: i32
+//! }
+//! ```
+//!
+//! # Merging
+//! When the  `merge_fn` argument is used `optfield` will add a method to the
+//! original struct that merges an opt struct back into the original.
+//!
+//! By default, the method is named `merge_opt` and has the following signature:
+//! ```
+//! # struct Opt;
+//! # impl Opt {
+//! // assuming the opt struct is named Opt
+//! fn merge_opt(&mut self, opt: Opt) // takes opt by value
+//! # {}
+//! # }
+//! ```
+//! When merging, all values of the opt struct that are not `None` are set as
+//! values of the original struct fields.
+//!
+//! To use it:
+//! ```
+//! # use optfield::*;
+//! #[optfield(Opt, merge_fn)]
+//! struct MyStruct {
+//!     text: String,
+//!     number: i32
+//! }
+//!
+//! let mut original = MyStruct {
+//!     text: "awesome".to_string(),
+//!     number: 1
+//! };
+//!
+//! let opt = Opt {
+//!     text: Some("amazing".to_string()),
+//!     number: None
+//! };
+//!
+//! original.merge_opt(opt);
+//!
+//! // text field value is merged
+//! assert_eq!(original.text, "amazing");
+//! // number field stays the same
+//! assert_eq!(original.number, 1);
+//! ```
+//! The merge function can be given:
+//! * custom name: `merge_fn = merge_other`
+//! * custom visibility (default is private): `merge_fn = pub(crate)`
+//! * both: `merge_fn = pub merge_other`
 extern crate proc_macro;
 
 use crate::proc_macro::TokenStream;
@@ -14,6 +413,11 @@ mod merge;
 use args::Args;
 use generate::generate;
 
+/// The macro
+///
+/// Check [crate documentation] for more information.
+///
+/// [crate documentation]: ./index.html
 #[proc_macro_attribute]
 pub fn optfield(attr: TokenStream, item: TokenStream) -> TokenStream {
     let item: ItemStruct = parse_macro_input!(item);

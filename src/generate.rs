38bf509e89eecfbb05d93430db1fb2f371e82181
Robert Ignat <robert.ignat91@gmail.com>
2020-04-03 13:45:19 +0300

    Add unit tests for main generate fn
    * sets_name
    * sets_generics

--- src/generate.rs
+++ src/generate.rs
@@ -22,3 +22,45 @@ pub fn generate(original: &ItemStruct, args: Args) -> TokenStream {
         #merge_impl
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::test_util::*;
+
+    #[test]
+    fn sets_name() {
+        let (item, args) = parse_item_and_args(
+            quote! {
+                struct S;
+            },
+            quote! {
+                Opt
+            },
+        );
+
+        let generated = parse_item(generate(&item, args));
+
+        assert_eq!(generated.ident, "Opt");
+    }
+
+    #[test]
+    fn sets_generics() {
+        let (item, args) = parse_item_and_args(
+            quote! {
+                struct S<'a, 'b, T, G> {
+                    t: &'a T,
+                    g: &'b G
+                }
+            },
+            quote! {
+                Opt
+            },
+        );
+
+        let generated = parse_item(generate(&item, args));
+
+        assert_eq!(item.generics, generated.generics);
+    }
+}
